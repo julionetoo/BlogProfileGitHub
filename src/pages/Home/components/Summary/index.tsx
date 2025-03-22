08e3091f82fd3f415d70eb5b55ae6604b6d566c3
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2025-03-21 21:35:20 -0300

    Atividade api github utilizando axios - aula 06

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -1,40 +1,67 @@
+import { api } from "../../../../lib/api";
 import { SummaryAnchors, SummaryContainer, SummaryHeader } from "./styles";
-
+import { useEffect, useState } from "react";
 import { ArrowUpRight, Buildings, GithubLogo, Users } from "phosphor-react";
+
+interface DadosPerfil {
+  avatar_url: string;
+  name: string;
+  bio: string;
+  login: string;
+  html_url: string;
+  company?: string;
+  followers: number;
+};
 
 
 export function Summary() {
 
-  // "https://api.github.com/users", "/lucaspedronet"
-  // "https://api.github.com/search"
-  // "https://api.github.com/repos/lucaspedronet/TudoLista/issues"
+  const [userData, setUserData] = useState<DadosPerfil | null>(null);
+
+  useEffect(() => {
+
+    api
+      .get("users/lucaspedronet")
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar os dados do GitHub:", error);
+      });
+
+  }, []);
+
+  if (!userData) {
+    return <p>Carregando...</p>;
+  }
+
 
   return (
     <SummaryContainer>
-      <img src="../src/assets/avatar.JPG" />
+      <img src={userData.avatar_url} />
       <section>
         <SummaryHeader>
-          <h1>Lucas Pedro</h1>
-          <a href="#" target="_blank">
+          <h1>{userData.name}</h1>
+          <a href={userData.html_url} target="_blank">
             GITHUB
             <ArrowUpRight size={12} />
           </a>
         </SummaryHeader>
-        <p>Software Engineering. developer at NodeJS, ReactJS, React Native, Electron.</p>
+        <p>{userData.bio}</p>
         <SummaryAnchors>
           <div>
             <GithubLogo size={18} />
-            <span>lucaspedronet</span>
+            <span>{userData.login}</span>
           </div>
 
           <div>
             <Buildings size={18} />
-            <span>Paraná Banco</span>
+            <span>{userData.company}</span>
           </div>
 
           <div>
             <Users size={18} />
-            <span>47</span>
+            <span>{userData.followers}</span>
           </div>
         </SummaryAnchors>
       </section>
